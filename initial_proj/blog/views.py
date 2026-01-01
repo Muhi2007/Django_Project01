22993d268efd4c6f3bf51382917c631f141e2844
Muhi2007 <muhammadteymurlu07@gmail.com>
2026-01-01 20:08:36 +0400

    Admin panel and database integration

--- initial_proj/blog/views.py
+++ initial_proj/blog/views.py
@@ -1,28 +1,11 @@
 from django.shortcuts import render
+from .models import Post
 
-posts = [
-    {
-        'username': 'Muhi',
-        'title': 'Blog Post 1',
-        'date': '30 December 2025',
-        'text': 'This is the first dummy post'
-    },
-    {
-        'username': 'Muhammad',
-        'title': 'Blog Post 2',
-        'date': '30 December 2025',
-        'text': 'This is the second dummy post'
-    },
-    {
-        'username': 'Eli',
-        'title': 'Blog Post 3',
-        'date': '30 December 2025',
-        'text': 'This is the third dummy post'
-    },
-]
+posts = Post.objects.all()
 
-# Create your views here.
 def home(request):
-    context = {'posts': posts}
+    context = {
+        'title': "Dummy",
+        'posts': posts}
 
     return render(request, "blog/home.html", context)
