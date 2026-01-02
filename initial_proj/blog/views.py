32ef7204871ecb77697fbd448f6f639fa3b443bc
Muhi2007 <muhammadteymurlu07@gmail.com>
2026-01-02 14:18:13 +0400

    Some improvements at visuals

--- initial_proj/blog/views.py
+++ initial_proj/blog/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 from .models import Post
 
-posts = Post.objects.all()
-
 def home(request):
     context = {
         'title': "Dummy",
-        'posts': posts}
+        'posts': Post.objects.all()}
 
     return render(request, "blog/home.html", context)
+
+def about(request):
+    return render(request, "blog/about.html")
