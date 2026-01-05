8aa3030b364bed4b8211ac17ad6795654a1c9f02
Muhi2007 <muhammadteymurlu07@gmail.com>
2026-01-05 14:20:28 +0400

    Classbased view

--- initial_proj/blog/views.py
+++ initial_proj/blog/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 from .models import Post
 
-def home(request):
-    context = {
-        'title': "Dummy",
-        'posts': Post.objects.all()}
 
-    return render(request, "blog/home.html", context)
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
+    ordering = ['-date']
+
+class PostDetailView(DetailView):
+    model = Post
+    
 
 def about(request):
     return render(request, "blog/about.html")
